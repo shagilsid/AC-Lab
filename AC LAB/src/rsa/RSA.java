ad4c88da9fbef8c9ae562c64f2e3d5b2073149fe
shagil <shagilsid@github.com>
2019-10-30 02:02:56 +0530

    Added Power method in RSA

--- AC LAB/src/rsa/RSA.java
+++ AC LAB/src/rsa/RSA.java
@@ -16,17 +16,30 @@ public class RSA {
 		
 		int d=calculateD(e,phi);
 		System.out.println("d: "+d);
-		int c=(int)(Math.pow(pt,e))%n;
-		int pp=(int)(Math.pow(c,d))%n;
-		System.out.println("Ecnrypted message is "+Math.pow(pt,e)%n);
+		long c=power(pt,e,n);
+		System.out.println("c: "+c);
+		long pp=power((int)c,d,n);
+		System.out.println("Ecnrypted message is "+c);
 		System.out.println("Decrypted message is "+pp);
 		
 
 
 	}
-	private static int calculateD(int e, int p) {
+
+	static long power(int a, int b, int q) {
+		long t;
+		if(b==1)
+			return a;
+		t=power(a,b/2,q);
+		if(b%2==0)
+			return (t*t)%q;
+		else
+			return (((t*t)%q)*a)%q;
+		
+	}
+	private static int calculateD(int e, int phi) {
 		int i=1;
-		while((e*i)%p!=1) {
+		while((e*i)%phi!=1) {
 			i++;
 		}
 		return i;
